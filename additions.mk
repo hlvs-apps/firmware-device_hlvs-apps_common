d6b9d10dbb79d5c0bc3f0f67c994b44e38006a2f
hlvs-apps <hlvs-apps@sauers.de>
2022-03-18 18:33:56 +0100

    Try to build Android Automotive for Galaxy S5mini

--- additions.mk
+++ additions.mk
@@ -1,12 +1,12 @@
 # Generic overlays
-PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/snappautomotive/common/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/generic/car/common/overlay device/hlvs-apps/common/overlay
 
 # Add non-public overlays if they exist
-$(call inherit-product-if-exists, vendor/snappautomotive/non_public/additions.mk)
+$(call inherit-product-if-exists, vendor/hlvs-apps/non_public/additions.mk)
 
 # Snapp Maps
 PRODUCT_PACKAGES += osmdroid
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    device/snappautomotive/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
+    device/hlvs-apps/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
