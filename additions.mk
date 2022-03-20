937a603117b4e9cf3d020077af79aeb49d8a05b8
hlvs-apps <hlvs-apps@sauers.de>
2022-03-20 14:50:58 +0100

    Update additions.mk

--- additions.mk
+++ additions.mk
@@ -6,7 +6,3 @@ $(call inherit-product-if-exists, vendor/hlvs-apps/non_public/additions.mk)
 
 # Snapp Maps
 PRODUCT_PACKAGES += osmdroid
-
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    device/hlvs-apps/common/bootanimations/bootanimation.zip:system/media/bootanimation.zip
